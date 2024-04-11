652880bd4e0b1f31ff80061a05a2c9da2f0d3f85
Andres Skrzyniecki <48762424+skrzynieckiUTN@users.noreply.github.com>
2024-04-10 23:49:46 -0300

    Agrego test, falta checkstyle

--- src/test/java/PrendaTest.java
+++ src/test/java/PrendaTest.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PrendaTest {
 
+private Prenda prendaDeTelaYColorPrimario(Tipo tipo) {
+  return new Prenda(tipo, Material.CUERO,new ColorRgb(15,325,9), null, null);
+}
+
 @Test
 void laCategoriaDeUnaBOTAesCALZADO() {
   assertEquals(prendaDeTelaYColorPrimario(Tipo.BOTAS).getCategoria(), Categoria.CALZADO);
@@ -21,16 +25,17 @@ void laCategoriaDeUnPANIUELOAesACCESTORIOS() {
   assertEquals(prendaDeTelaYColorPrimario(Tipo.PANIUELO).getCategoria(), Categoria.ACCESORIOS);
 }
 
-private Prenda prendaDeTelaYColorPrimario(Tipo tipo) {
-  return new Prenda(tipo, Material.CUERO,new ColorRgb(15,325,9), null, null);
-}
-
 @Test
 void prendaSinMaterialNoSePuedeCrear() { //se lanza la null pointer exception
   assertThrows(NullPointerException.class,
       ()->{
         new Prenda(null, Material.CUERO,new ColorRgb(15,325,9), null, null);
       });
+}
+
+@Test
+  void remeraNoPuedeSerParteInferior() {
+    assertNotEquals(prendaDeTelaYColorPrimario(Tipo.REMERA).getCategoria(), Categoria.CALZADO);
 }
 
 }
